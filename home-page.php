47074f07660fb6b22123bd87d2a0296f0b5b783f
Marko Babovic <babovic.marko@yahoo.com>
2019-10-18 12:49:36 +0200

    First commit

---
+++ home-page.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta charset="utf-8">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+
+
+    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="js/global.js"></script>
+</body>
+</html>
---
+++ js/global.js
@@ -0,0 +1,23 @@
+// for wordpress  jQuery(function ($) {
+(function() {
+
+
+    $(window).on('load', function(){
+        $("body").addClass("load-done");
+    });
+
+    $(document).scroll(function(e){
+
+    });
+
+    $(window).resize(function(){
+
+    });     
+
+
+})();
+
+// for wordpress });
+
+
+
